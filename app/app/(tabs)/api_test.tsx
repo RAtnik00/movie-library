80d163ebf5f47bad5f132d3dd53a97435964d7b7
Andrii Moskalenko <andriy.mos.my@gmail.com>
2026-03-24 19:39:27 +0100

    Small changes and practise

---
+++ app/app/(tabs)/api_test.tsx
@@ -0,0 +1,126 @@
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export default function ApiTest() {
+  const [films, setFilms] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchFilms = async () => {
+    try {
+      const response = await fetch("http://fakestoreapi.com/products");
+      const json = await response.json();
+      setFilms(json);
+    } catch (err) {
+      setError("Failed to fetch films");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFilms();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text>{error}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Some random API</Text>
+
+      <FlatList
+        data={films}
+        keyExtractor={(film) => film.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <View>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.description}>{item.description}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    padding: 10,
+    marginTop: 60,
+  },
+
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 15,
+    textAlign: "center",
+  },
+
+  card: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 8,
+    elevation: 2,
+  },
+
+  image: {
+    width: 80,
+    height: 80,
+    resizeMode: "contain",
+    marginRight: 15,
+  },
+
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  price: {
+    fontSize: 18,
+    color: "green",
+    marginTop: 5,
+  },
+  description: {
+    fontSize: 14,
+    color: "#555",
+  },
+});
