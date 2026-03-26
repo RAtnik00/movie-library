4b7e273a169d04a018f0baaf87d016dc3e92c175
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-03-26 13:25:16 +0100

    Use environment variables for database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,16 @@
+import os
 import psycopg
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     conn = psycopg.connect(
-        dbname='movie_library',
-        user='movie_user',
-        password='1234',
-        host='localhost',
-        port='5432',
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
     )
     return conn
-
-if __name__ == "__main__":
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT 1;")
-    result = cur.fetchall()
-    print(result)
-    cur.close()
-    conn.close()
