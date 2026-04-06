b77b63ce1f48c9f55180feaa541546dfab2a0ae9
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-04-06 15:52:12 +0200

    Add get_current_user and JWT auth dependency

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,12 +1,17 @@
 import os
 from datetime import datetime, timedelta, timezone
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 from pwdlib import PasswordHash
 from fastapi.security import OAuth2PasswordBearer
 from starlette import status
+
+from app.database import get_db
+from app.models.user import User
+
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
@@ -17,6 +22,17 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+    user_id = decode_access_token(token)
+    user = db.get(User, user_id)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
+
+    return user
 
 
 def get_password_hash(password: str) -> str:
@@ -36,7 +52,7 @@ def create_access_token(data: dict) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def decode_access_token(token: str):
+def decode_access_token(token: str) -> int:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         user_id = payload.get("sub")
@@ -47,7 +63,7 @@ def decode_access_token(token: str):
             )
 
         return int(user_id)
-    
+
     except JWTError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
