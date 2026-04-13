47d9f5ed93aadad4576d0cf4377591a91bf93478
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-04-13 19:42:50 +0200

    add TMDB client and implement GET /movies (popular)

---
+++ backend/app/routes/movies.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from fastapi import Depends
+
+from app.dependencies.movies import get_movies_api_client
+from app.services.movies_api import MoviesAPIClient
+
+router = APIRouter()
+
+@router.get("/movies")
+def get_movies(client: MoviesAPIClient = Depends(get_movies_api_client)):
+    return client.get_popular()
---
+++ backend/app/services/movies_api.py
@@ -0,0 +1,13 @@
+import httpx
+
+
+class MoviesAPIClient:
+    def __init__(self, base_url: str, api_key: str):
+        self.base_url = base_url
+        self.api_key = api_key
+
+    def get_popular(self):
+        url = f"{self.base_url}/movie/popular"
+        params = {"page": 1, "api_key": self.api_key}
+        response = httpx.get(url, params=params)
+        return response.json()
